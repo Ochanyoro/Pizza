634ebd989a6f07e86ea4f8a74b31a039b773574c
Ochanyoro <wadaoo0223@icloud.com>
2022-07-29 23:07:45 +0900

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by 大和田一裕 on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
